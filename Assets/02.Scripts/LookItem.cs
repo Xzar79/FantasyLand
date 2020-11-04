76925ed5b00cab80aa3a1f414dd4fa27aa1b502a
Xzar79 <Xzar79@users.noreply.github.com>
2020-11-04 10:52:10 +0900

    fda
    fda

---
+++ Assets/02.Scripts/LookItem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LookItem : MonoBehaviour, IPointerEnterHandler,
+    IPointerExitHandler, IGvrPointerHoverHandler
+{
+
+    private Animator anim;
+    private readonly int hashIsOpen = Animator.StringToHash("IsOpen");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void OnBoxOpen(bool isOpen)
+    {
+        anim.SetBool(hashIsOpen, isOpen);
+    }
+
+    public void OnLookItemBox(bool isLookAt)
+    {
+        Debug.Log(isLookAt);
+        MoveCtrl.isStopped = isLookAt;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MoveCtrl.isStopped = true;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        MoveCtrl.isStopped = false;
+    }
+    public void OnGvrPointerHover(PointerEventData eventData)
+    {
+        Debug.Log("Reticle On");
+    }
+}
